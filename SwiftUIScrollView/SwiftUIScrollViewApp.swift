adc2e18910959d724ec734aeb86db18335444e17
Carlos Kimura <ckimura@gmail.com>
2025-01-22 21:37:35 -0300

    initial commit

---
+++ SwiftUIScrollView/SwiftUIScrollViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIScrollViewApp.swift
+//  SwiftUIScrollView
+//
+//  Created by Carlos Kimura on 22/01/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIScrollViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
